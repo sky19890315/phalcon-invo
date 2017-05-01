9f00cb35c9c4aa20ed07fd92722c39caf654ac87
sunkeyi <296675685@qq.com>
2017-05-01 19:35:09 +0800

    第一次提交

---
+++ app/controllers/ControllerBase.php
@@ -0,0 +1,13 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class ControllerBase extends Controller
+{
+
+    protected function initialize()
+    {
+        $this->tag->prependTitle('INVO | ');
+        $this->view->setTemplateAfter('main');
+    }
+}
